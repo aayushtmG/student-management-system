dbe5bed2014308fa88d335a1e6e316bad460b1c5
aayushtmG <aiyoustmg@gmail.com>
2026-05-04 17:55:23 +0545

    levelcontroller updated

--- src/main/java/com/aayush/student_management/controller/LevelController.java
+++ src/main/java/com/aayush/student_management/controller/LevelController.java
@@ -1,16 +1,28 @@
 package com.aayush.student_management.controller;
 
+import com.aayush.student_management.dto.level.LevelResponseDto;
+import com.aayush.student_management.service.LevelService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/levels")
 public class LevelController {
 
-//    @GetMapping
-//    public ResponseEntity<Level> getAllLevels(){
-//
-//    }
+    public final LevelService levelService;
+
+    public LevelController(LevelService levelService) {
+        this.levelService = levelService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LevelResponseDto>> getAllLevels(){
+          return new ResponseEntity<>(levelService.getAllLevels(), HttpStatus.OK);
+    }
 
 }
